1d82282cde7730597c6ea6e28e07de219a41406d
stijn heynderickx <stijn.heynderickx@gmail.com>
2018-01-10 11:37:55 +0100

    proberen synce met de MOD ESP8226 DEV wat absoluut niet vanzelf gaat

--- HelloServer_push_stijn/HelloServer_push_stijn.ino
+++ HelloServer_push_stijn/HelloServer_push_stijn.ino
@@ -1,4 +1,23 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#include <WiFiEsp.h>
+
+#include <WiFiEspServer.h>
+//#include <WiFiEspUdp.h>
+
+//#include <ESP8266wifi.h>
+
+
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
@@ -48,6 +67,7 @@ void setup(void){
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
+  
   WiFi.begin(ssid, password);
   Serial.println("");
 
